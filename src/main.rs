57fc23f486305b732b71795470e734869678298d
Thomas Fasan <thomas.fasan2@gmail.com>
2023-09-21 08:19:22 -0400

    after

--- src/main.rs
+++ src/main.rs
@@ -100,25 +100,24 @@ enum Pickle {
     Yellow
 }
 
-fn make_board(pickles:Vec<(u8,u8,Pickle)>) -> Board {
+fn make_board(pickles:Vec<(u32,u32,Pickle)>) -> Board {
     let mut r = Board::new();
     for (x,y,pickle) in pickles {
         
-        let row = r.get_mut(y.into());
+        let row = r.get_mut(y);
         *row = *row | make_row(x,pickle);
     }
     r
 }
 
-fn make_row(x:u8,pickle:Pickle) -> u32 {
+fn make_row(x:u32,pickle:Pickle) -> u32 {
     let mut r:u32 = match pickle {
-        Pickle::White => 8,
-        Pickle::Purple => 12,
-        Pickle::Green => 10,
+        Pickle::White => 1,
+        Pickle::Purple => 3,
+        Pickle::Green => 5,
         Pickle::Yellow => 9
     };
     r = r << (x*4);
-    r = r.reverse_bits();
     r
 }
 
@@ -126,124 +125,143 @@ fn make_row(x:u8,pickle:Pickle) -> u32 {
     
 // }
 
-pub fn elim_mosaic_breaking_moves(board:&Board, moves:&mut Moves) -> Moves {
+// const mosaic_colors:[u32; 9] = [0x333,0x555,0x999,0x359,0x593,0x935, 0x395,0x953,0x539];
+// const mosaic_colors_2:[u8; 6] = [0x33,0x55,0x99,0x59,0x93,0x39,0x95,0x53]; 
 
-    for r in 0..11 {
-        //first 8 nibbles is one of the row. Second is for the row above/below it.
-        //merged into 64bits bc uh idk
-        let mut row:u64 = (board.get(r) as u64).rotate_left(32) | (if r<10 { board.get(r+1) } else { 0 }) as u64;
-        let mut row_lower:u32 = if r>0 { board.get(r-1) } else { 0 };
+pub fn elim_mosaic_breaking_moves(x:u32,y:u32,board:&Board) -> u32 {
+    let mut r0 = if y<10 { board.get(y+1) } else { 0 };
+    let mut r1 = board.get(y);
+    let mut r2 = if y>0 { board.get(y-1) } else { 0 };
+    let mut r3 = if y>1 { board.get(y-2) } else { 0 };
 
-        // let mut moves_row:u32 = moves.get(r);
-        // let mut moves_lower:u32 = if r>0 { moves.get(r-1) } else { 0 };
-        // let mut moves_upper:u32 = if r<10 { board.get(r+1) } else { 0 };
-        // let mut moves_upper_upper:u32 = if r<9 { board.get(r+2) } else { 0 };
-        for up in 1..=2 {
-            /*
-            first N is at idx 0, when it shifts there needs to be an N where the star is.
-            in the hex in this case C means the color bit, so for purple 2, green 4, and yellow 88
-            E* = wraparound
-            assumes board is valid(no hanging)
-             N N    upper-upperso count ones only has to do one thing
-            N C N  != E0E00000 & 0C000000 upper
-            *C C N != 00E0000E*& CC000000 row
-            N N N  != EEE00000 lower
-            */
+    println!("r0: {:#016x}",r0);
+    println!("r1: {:#016x}",r1);
+    println!("r2: {:#016x}",r2);
+    println!("r3: {:#016x}",r3);
 
-            /*
-            if row is at one of the odd numbers then its like this and theres quite a bit of overflow
-            N N
-                C N
-            C C N
-                N N
-            */
-
-            //if no surrounding spots are colored. it is okay for the E to be at the end, because the largest row is 7 long
-            
-            let mut color_mask:u64 =     0xFF000000_0F000000;
-            let mut valid_purple:u64 =   0x22000000_02000000;
-            let mut valid_green:u64 =    0x44000000_04000000;
-            let mut valid_yellow:u64 =   0x88000000_08000000;
-            let mut not_kernel:u64 =     0x00E00000_E0E0000E; //<-overflow
-            let mut not_kernel_lower:u32=0xEEE00000;
-
-            let mut valid_1:u64 =        0x24000000_08000000;
-            let mut valid_2:u64 =        0x42000000_08000000;
-            let mut valid_3:u64 =        0x82000000_04000000;
-            let mut valid_4:u64 =        0x84000000_02000000;
-            let mut valid_5:u64 =        0x48000000_02000000;
-            let mut valid_6:u64 =        0x28000000_04000000;
-
-            let mut breaking_lower:u32 = 0x111FFFFF;
-            let mut breaking:u32 =       0xFF1FFFF1;
-            let mut breaking_upper:u32 = 0x1F1FFFFF;
-            let mut breaking_upper_upper:u32 = 0x11FFFFFF;
-
-            if r%2==1 {
-                color_mask = 0xFF000000_F0000000;
-                valid_purple=0x22000000_20000000;
-                valid_green= 0x44000000_40000000;
-                valid_yellow=0x80000000_80000000;
-                not_kernel = 0x00E0000E_0E00000E;
-                not_kernel_lower= 0xEE00000E;
-
-                valid_1 =        0x24000000_80000000;
-                valid_2 =        0x42000000_80000000;
-                valid_3 =        0x82000000_40000000;
-                valid_4 =        0x84000000_20000000;
-                valid_5 =        0x48000000_20000000;
-                valid_6 =        0x28000000_40000000;
-
-                breaking_lower = 0x11FFFFF1;
-                breaking =       0xFF1FFFF1;
-                breaking_upper = 0xF1FFFFF1;
-                breaking_upper_upper = 0x11FFFFF1;
-            }
-
-            for _ in 1..=(4+r%2) {
-                let working = color_mask & row;
-
-                //checks if there is mosaic
-                if (row_lower & not_kernel_lower) == 0 && (not_kernel & row) == 0 && (
-                    working==valid_purple || working==valid_green || working==valid_yellow
-                    || working==valid_1 || working==valid_2 || working==valid_3 
-                    || working==valid_4 || working==valid_5 || working==valid_6
-                ) {
-                    //bans moves that will break that mosaic
-                    if r>0 { *moves.get_mut(r-1) = moves.get(r-1) & breaking_lower; }
-                    *moves.get_mut(r) = moves.get(r) & breaking; //again, okay to set the last pixel to whatever because it isint being used.
-                    if r<10 { *moves.get_mut(r+1) = moves.get(r+1) & breaking_upper; }
-                    if r<9 { *moves.get_mut(r+2) = moves.get(r+2) & breaking_upper_upper; }
-                }
-
-                //logical shift instead of arithmetic bc the thingy needs to wrap around.
-                color_mask = color_mask.rotate_right(4);
-                valid_purple = valid_purple.rotate_right(4);
-                valid_green = valid_green.rotate_right(4);
-                valid_yellow = valid_yellow.rotate_right(4);
-                not_kernel = not_kernel.rotate_right(4);
-                not_kernel_lower = not_kernel_lower.rotate_right(4);
-
-                valid_1 = valid_1.rotate_right(4);
-                valid_2 = valid_2.rotate_right(4);
-                valid_3 = valid_3.rotate_right(4);
-                valid_4 = valid_4.rotate_right(4);
-                valid_5 = valid_5.rotate_right(4);
-                valid_6 = valid_6.rotate_right(4);
-
-                breaking_lower = breaking_lower.rotate_right(4);
-                breaking = breaking.rotate_right(4);
-                breaking_upper = breaking_upper.rotate_right(4);
-                breaking_upper_upper = breaking_upper_upper.rotate_right(4);
-            }
-
-            row = (board.get(r) as u64).rotate_left(32) | (if r>0 { board.get(r-1) } else { 0 }) as u64;
-            row_lower = if r<10 { board.get(r+1) } else { 0 };
-        }        
+    //shift on a unsigned integer is a logical shift, so bits will be deleted.
+    let mask0:u32 = 0x0000FFFFu32;
+    let mask1:u32 = 0x000FFFFFu32;
+    let mask2:u32 = 0x00000FFFu32;
+    
+    if x>2 { 
+        r0 = r0 >> (x-2);
+        r1 = r1 >> (x-2);
+        r2 = r2 >> (x-2);
+        r3 = r3 >> (x-2);
+    } else if x<2 { 
+        r0 = r0 << (2-x);
+        r1 = r1 << (2-x);
+        r2 = r2 << (2-x);
+        r3 = r3 << (2-x);
     }
 
-    return moves.clone();
+    r3 = r3 >> 4;
+
+    if y%2==0 {
+        r0 = r0 >> 4;
+        r2 = r2 >> 4;
+    }
+
+    println!("r0: {:#016x}",r0);
+    println!("r1: {:#016x}",r1);
+    println!("r2: {:#016x}",r2);
+    println!("r3: {:#016x}",r3);
+
+    let working:u64 = (mask0 & r0) as u64 | (((mask1 & r1) as u64) << 4*4) | (((mask0 & r2) as u64) << 9*4) | (((mask2 & r3) as u64) << 13*4);
+
+    println!("Working: {:#018x}",working);
+
+    //x,y is the last pixel to complete a mosaic
+    let v1_and_0:u64 =    0xEEE_E00E_0E0E0_0000;
+    let v1_color_1:u64 =  0x000_0E00_00000_0000;
+    let v1_color_2:u64=   0x000_00E0_00000_0000;
+
+    let v2_and_0:u64 =    0x0EE_0E0E_0E00E_000E;
+    let v2_color_1:u64 =  0x000_0000_000E0_0000;
+    let v2_color_2:u64=   0x000_00E0_00000_0000;
+
+    let v3_and_0:u64 =    0xEE0_E0E0_E00E0_E000;
+    let v3_color_1:u64 =  0x000_0E00_00000_0000;
+    let v3_color_2:u64 =  0x000_0000_0E000_0000;
+
+    if v1_and_0 & working == 0 && v1_color_1 & working != 0 && v1_color_2 & working != 0 {
+        let color_1 = (working & v1_color_1) >> 11*4;
+        let color_2 = (working & v1_color_2) >> 10*4;
+        println!("COLORS: {}, {}",color_1,color_2);
+        let missing_color = if color_1 == color_2 { color_1 } else { 0xE - color_1 - color_2 };
+        
+        //make completing the mosaic the only legal move
+        return missing_color as u32;
+    }
+
+    if v2_and_0 & working == 0 && v2_color_1 & working != 0 && v2_color_2 & working != 0 {
+        let color_1 = (working & v2_color_1) >> 5*4;
+        let color_2 = (working & v2_color_2) >> 10*4;
+        let missing_color = if color_1 == color_2 { color_1 } else { 0xE - color_1 - color_2 };
+        
+        //make completing the mosaic the only legal move
+        return missing_color as u32;
+    }
+
+    if v3_and_0 & working == 0 && v3_color_1 & working != 0 && v3_color_2 & working != 0 {
+        let color_1 = (working & v3_color_1) >> 11*4;
+        let color_2 = (working & v3_color_2) >> 7*4;
+        let missing_color = if color_1 == color_2 { color_1 } else { 0xE - color_1 - color_2 };
+        
+        //make completing the mosaic the only legal move
+        return missing_color as u32;
+    }
+    
+    let w1_and_0 = 0xEE0_E000_0E000_0000;
+    let w1_color = 0x000_0E00_00000_0000;
+
+    let w2_and_0 = 0x000_EE00_E0000_E000;
+    let w2_color = 0x000_0000_0E000_0000;
+    
+    let w3_and_0 = 0x0EE_0E0E_000EE_0000;
+    let w3_color = 0x000_00E0_00000_0000;
+
+    let w4_and_0 = 0x000_00EE_0000E_000E;
+    let w4_color = 0x000_0000_000E0_0000;
+
+    //x,y is touching two colored tiles
+    let mut touching = 0;
+    if w1_color & working != 0 { touching += 1; }
+    if w2_color & working != 0 { touching += 1; }
+    if w3_color & working != 0 { touching += 1; }
+    if w4_color & working != 0 { touching += 1; }
+    
+    println!("TOUCHING={}",touching);
+
+    if touching > 1 {
+        return 0x1;
+    }
+
+    println!("w1={:#018x} {:#018x}",w1_and_0 & working,w1_color & working);
+
+    //x,y is bordering a colored tile connected to another one already
+    if (w1_and_0 & working != 0 && w1_color & working != 0) ||
+    (w2_and_0 & working != 0 && w2_color & working != 0) ||
+    (w3_and_0 & working != 0 && w3_color & working != 0) ||
+    (w4_and_0 & working != 0 && w4_color & working != 0) {
+       return 0x1;
+    }
+
+    //x,y has a colored tile under it but the 3rd space for mosaic is covered by white
+    let o2 =    0x000_0000_0F000_0000;
+    let o2_eq = 0x000_0000_01000_0000;
+    let o4 =    0x000_0000_000F0_0000;
+    let o4_eq = 0x000_0000_00010_0000;
+    if (w1_color & working != 0 && o2 & working == o2_eq) || (w3_color & working != 0 && o4 & working == o4_eq) {
+        return 0x1;
+    }
+
+    return 0xF;
+
 }
+
+//PRINTING STUFF
 
 const PRINT_PADDING:usize = 4;
 fn print_board(board:&Board) {
@@ -271,11 +289,8 @@ fn print_moves(moves:&Moves) {
 }
 
 fn get_column(row:u32,x:u32) -> String {
-    
-    let mask:u32 = 0xF0000000 >> (x*4);
-    // println!("{:032b}, {:032b}, {}",row,mask,x);
-    let mut res = mask & row;
-    res = res >> (32-(x+1)*4);
+    let mask:u32 = 0x0000000F;
+    let res = (row >> (x*4)) & mask;
     return format!("{:04b}",res);
 }
 
@@ -292,13 +307,18 @@ fn color_pixel(pixel:String) -> ColoredString {
 
 fn main() {
     println!("Hello, world!");
-    let pickles = vec![(0,0,Pickle::Green),(1,0,Pickle::Green),(1,1,Pickle::Green)];
+    // let pickles = vec![(1,1,Pickle::White),(2,1,Pickle::White),(3,1,Pickle::White),
+    // (4,2,Pickle::Purple),(1,2,Pickle::Purple),(2,2,Pickle::Purple),(3,2,Pickle::Purple),
+    // (0,3,Pickle::Green),(1,3,Pickle::Green),(3,3,Pickle::Green),(4,3,Pickle::Green),
+    // (1,4,Pickle::Yellow),(4,4,Pickle::Yellow)];
+
+    let pickles = vec![(1,2,Pickle::White),(2,1,Pickle::Green),(1,1,Pickle::White)];
     let board = make_board(pickles);
     let mut moves = Moves::ones();
 
-    elim_mosaic_breaking_moves(&board, &mut moves);
-
+    let result = elim_mosaic_breaking_moves(2,2,&board);
+    
     print_board(&board);
     println!("{}","=".repeat(15*PRINT_PADDING));
-    print_moves(&moves);
+    println!("result is: {:04b}",result);
 }
